acdfa6b5d565542b0abc4671d8a51b7e67362df2
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-08-18 16:14:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ function filterByQuery(query, animalsArray) {
     // return the filtered results:
     return filteredResults;
   }
+
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
   app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -50,9 +56,14 @@ function filterByQuery(query, animalsArray) {
   });
 
   app.get('/api/animals/:id', (req, res) => {
-    const result = findByPk(req.params.id, animals);
+    const result = findById(req.params.id, animals);
+    if (result) {
       res.json(result);
+    } else {
+      res.send(404);
+    }
   });
+
   
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
